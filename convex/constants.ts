5dd1b93d9ca4fb27804ec505ddc5fb4ddd7800dc
akash202004 <akashlaha48@gmail.com>
2024-11-25 12:20:04 +0530

    event filtering and making the waitng queue

---
+++ convex/constants.ts
@@ -0,0 +1,22 @@
+import { Doc } from "./_generated/dataModel";
+
+// Time constants in milliSeconds
+export const DURATION = {
+  TICKET_OFFER: 15 * 60 * 1000, // Minimum stripe allow to payment expiry
+} as const;
+
+// status type for better type safety
+export const WAITING_LIST_STATUS: Record<string, Doc<"waitingList">["status"]> =
+  {
+    WAITING: "waiting",
+    OFFERED: "offered",
+    PURCHASED: "purchased",
+    EXPIRED: "expired",
+  } as const;
+
+export const TICKET_STATUS: Record<string, Doc<"tickets">["status"]> = {
+  VALID: "valid",
+  USED: "used",
+  REFUNDED: "refunded",
+  CANCELLED: "cancelled",
+} as const;
--- convex/events.ts
+++ convex/events.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
 import { query } from "./_generated/server";
+import { TICKET_STATUS, WAITING_LIST_STATUS } from "./constants";
 
 export const get = query({
   args: {},
@@ -22,15 +23,34 @@ export const getEventAvailability = query({
   args: { eventId: v.id("events") },
   handler: async (ctx, { eventId }) => {
     const event = await ctx.db.get(eventId);
-    if(!event){
+    if (!event) {
       throw new Error("Event not found");
     }
 
     // Count total purchased tickets
     const purchasedTickets = await ctx.db
-    .query("tickets")
-    .withIndex("by_event", (q)=>q.eq("eventId", eventId))
-    .collect()
-    .then((ticket)=>ticket.filter((t)=>t.status ===))
+      .query("tickets")
+      .withIndex("by_event", (q) => q.eq("eventId", eventId))
+      .collect()
+      .then(
+        (ticket) =>
+          ticket.filter(
+            (t) =>
+              t.status === TICKET_STATUS.valid ||
+              t.status === TICKET_STATUS.USED
+          ).length
+      );
+
+    // Count current valid tickets
+    const now = Date.now();
+    const activeOffers = await ctx.db
+      .query("waitingList")
+      .withIndex("by_event_status", (q) =>
+        q.eq("eventId", eventId).eq("status", WAITING_LIST_STATUS.OFFERED)
+      )
+      .collect()
+      .then(
+        (entries) => entries.filter((e) => (e.offerExpiresAt ?? 0) > now).length
+      );
   },
 });

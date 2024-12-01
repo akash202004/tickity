a3b942e354b9069d1831ff525769d36b87585402
akash202004 <akashlaha48@gmail.com>
2024-12-01 17:32:06 +0530

    adding people on queue after checking certain criteria

--- convex/events.ts
+++ convex/events.ts
@@ -1,6 +1,7 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
-import { TICKET_STATUS, WAITING_LIST_STATUS } from "./constants";
+import { DURATION, TICKET_STATUS, WAITING_LIST_STATUS } from "./constants";
+import { internal } from "./_generated/api";
 
 // get all events
 export const getAll = query({
@@ -39,7 +40,7 @@ export const getEventAvailability = query({
         (ticket) =>
           ticket.filter(
             (t) =>
-              t.status === TICKET_STATUS.valid ||
+              t.status === TICKET_STATUS.VALID ||
               t.status === TICKET_STATUS.USED
           ).length
       );
@@ -57,9 +58,11 @@ export const getEventAvailability = query({
       );
 
     const totalReserved = purchasedCount + activeOffers;
+    const availableSpots = event.totalTickets - (purchasedCount + activeOffers);
 
     return {
       isSoldOut: totalReserved >= event.totalTickets,
+      available: availableSpots > 0,
       totalTickets: event.totalTickets,
       purchasedCount,
       activeOffers,
@@ -68,8 +71,104 @@ export const getEventAvailability = query({
   },
 });
 
+// // helper function to check ticket availability for an event
+// export const checkTicketAvailability = query({
+//   args: { eventId: v.id("events") },
+//   handler: async (ctx, { eventId }) => {
+//     const event = await ctx.db.get(eventId);
+//     if (!event) {
+//       throw new Error("Event not found");
+//     }
+
+//     // Count total purchased tickets
+//     const purchasedCount = await ctx.db
+//       .query("tickets")
+//       .withIndex("by_event", (q) => q.eq("eventId", eventId))
+//       .collect()
+//       .then(
+//         (ticket) =>
+//           ticket.filter(
+//             (t) =>
+//               t.status === TICKET_STATUS.WALID ||
+//               t.status === TICKET_STATUS.USED
+//           ).length
+//       );
+
+//     // Count current valid tickets
+//     const now = Date.now();
+//     const activeOffers = await ctx.db
+//       .query("waitingList")
+//       .withIndex("by_event_status", (q) =>
+//         q.eq("eventId", eventId).eq("status", WAITING_LIST_STATUS.OFFERED)
+//       )
+//       .collect()
+//       .then(
+//         (entries) => entries.filter((e) => (e.offerExpiresAt ?? 0) > now).length
+//       );
+
+//     const availableSpots = event.totalTickets - (purchasedCount + activeOffers);
+
+//     return {
+//       available: availableSpots > 0,
+//       availableSpots,
+//       totalTickets: event.totalTickets,
+//       purchasedCount,
+//       activeOffers,
+//     };
+//   },
+// });
+
 // join the waiting list for an event
 export const joinWaitingList = mutation({
   args: { eventId: v.id("events"), userId: v.string() },
-  handler: async (ctx, { eventId, userId }) => {},
+  handler: async (ctx, { eventId, userId }) => {
+    // check rate limit
+    // const status = await rateLimiter.limit(ctx, "queueJoin", { key: userId });
+    // if (!status.ok) {
+    //   throw new ConvexError(
+    //     `You've joined the waiting list too many times. Please wait ${Math.ceil(status.retryAfter / (60 * 1000))} seconds before trying again.`
+    //   );
+    // }
+
+    // check if user already has an active entry in waiting list for this event
+    // Active ,eams any status except EXPIRED
+    const existingEntry = await ctx.db
+      .query("waitingList")
+      .withIndex("by_user_event", (q) =>
+        q.eq("userId", userId).eq("eventId", eventId)
+      )
+      .filter((q) => q.neq("status", WAITING_LIST_STATUS.EXPIRED))
+      .first();
+
+    if (existingEntry) {
+      throw new Error("You're already on the waiting list for this event");
+    }
+
+    // Verify the event exists
+    const event = await ctx.db.get(eventId);
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    // check if there are any available tickets right now
+    const { available } = await getEventAvailability(ctx, { eventId });
+    const now = Date.now();
+
+    if (available) {
+      // Create a new entry in the waiting list
+      const waitingListId = await ctx.db.insert("waitingList", {
+        eventId,
+        userId,
+        status: WAITING_LIST_STATUS.WAITING,
+        offerExpiresAt: now + DURATION.TICKET_OFFER,
+      });
+
+      // schedule a job to expire the ticket offer after the offer duration
+      await ctx.scheduler.runAfter(
+        DURATION.TICKET_OFFER,
+        internal.waitingList.expireOffer,
+        { waitingListId, eventId }
+      );
+    }
+  },
 });

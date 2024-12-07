eec63f1433ee1f9002a925ae3052a673591ba4c3
akash202004 <akashlaha48@gmail.com>
2024-12-07 09:44:08 +0530

    stripe checkout session almost done

--- actions/createStripeCheckoutSession.ts
+++ actions/createStripeCheckoutSession.ts
@@ -2,6 +2,8 @@
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { DURATION } from "@/convex/constants";
+import { stripe } from "@/lib/stripe";
 import { auth } from "@clerk/nextjs/server";
 
 export type StripeCheckoutMetaData = {
@@ -40,4 +42,40 @@ export async function createStripeCheckoutSession({
   if (!queuePosition.offerExpiresAt) {
     throw new Error("Ticket offer has no expiration date");
   }
+
+  const metadata: stripeCheckoutMetaData = {
+    eventId,
+    userId,
+    waitingListId: queuePosition._id,
+  };
+
+  const session = await stripe.checkout.sessions.create(
+    {
+      payment_method_types: ["card", "paypal"],
+      line_items: [
+        {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: event.name,
+              description: event.description,
+            },
+            unit_amount: Math.round(event.price * 100),
+          },
+          quantity: 1,
+        },
+      ],
+      payment_intent_data: {
+        application_fee_amount: Math.round(event.price * 100 * 0.01),
+      },
+      expires_at: Math.floor(Date.now() / 1000) + DURATION.TICKET_OFFER / 1000,
+      mode: "payment",
+      success_url: `${baseUrl}/tickets/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${baseUrl}/event/${eventId}`,
+      metadata,
+    },
+    { stripeAccount: stripeConnectId }
+  );
+
+  return { sessionId: session.id, sessionUrl: session.url };
 }

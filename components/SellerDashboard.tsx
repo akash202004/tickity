bae3d6a8ddd19994a60a2fac22850e1fc4f95186
akash202004 <akashlaha48@gmail.com>
2024-12-07 22:11:49 +0530

    setup the seller dashboard

--- components/SellerDashboard.tsx
+++ components/SellerDashboard.tsx
@@ -1,9 +1,29 @@
-"use client"
+"use client";
+
+import { AccountStatus } from "@/actions/getStripeConnectAcccountStatus";
+import { api } from "@/convex/_generated/api";
+import { useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 function SellerDashboard() {
-  return (
-    <div>SellerDashboard</div>
-  )
+  const [accountCreatePending, setAccountCreatePending] = useState(false);
+  const [accountLinkCreatePending, setAccountLinkCreatePending] =
+    useState(false);
+  const [error, setError] = useState(false);
+  const [accountStatus, setAccountStatus] = useState<AccountStatus | null>(
+    null
+  );
+  const router = useRouter();
+  const { user } = useUser();
+  const stripeConnectId = useQuery(api.users.getUserStripeConnectId, {
+    userId: user?.id || "",
+  });
+  const isReadyToAcceptPayments =
+    accountStatus?.isActive && accountStatus?.payoutsEnabled;
+
+  return <div>hi</div>;
 }
 
-export default SellerDashboard
+export default SellerDashboard;

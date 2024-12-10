0a8078a5be2f7bf58d8c2cf1e15c0c14c7d341e9
akash202004 <akashlaha48@gmail.com>
2024-12-10 14:30:00 +0530

    adding the component from shadcn WIP

---
+++ components/EventForm.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { Id } from "@/convex/_generated/dataModel"
+
+export default function EventForm({eventId}: {eventId: Id<"events">}) {
+  return (
+    <div>EventForm</div>
+  )
+}
